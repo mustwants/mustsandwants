c31b8e178ff020b833efe61b38ec3b721801da87
MustWants <90696663+mustwants@users.noreply.github.com>
2025-10-29 10:08:45 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,15 +3,26 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        mwGreen: "#8ABA4E",
+        mwAqua: "#74E4B8",
+        mwLime: "#D1EE00",
+        mwPink: "#F20359",
+        mwBlack: "#000000",
+        mwWhite: "#FFFFFF",
+      },
+      fontFamily: {
+        heading: ['"Hoglar Bold"', "sans-serif"],
+        subheading: ['"Roboto Condensed"', "sans-serif"],
+        body: ["Roboto", "sans-serif"],
+      },
+      gradientColorStops: {
+        mw: ["#74E4B8", "#D1EE00"],
       },
     },
   },
@@ -19,4 +30,3 @@ const config: Config = {
 };
 
 export default config;
-
